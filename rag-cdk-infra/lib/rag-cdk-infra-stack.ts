c66877d9cb9f6c5c77e28b2a3775ccfdb1f1160a
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-06-24 18:24:03 -0400

    FIX: Finally fixed to include the infra file

---
+++ rag-cdk-infra/lib/rag-cdk-infra-stack.ts
@@ -0,0 +1,59 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import {
+  DockerImageFunction,
+  DockerImageCode,
+  FunctionUrlAuthType,
+  Architecture,
+} from "aws-cdk-lib/aws-lambda";
+import { ManagedPolicy } from "aws-cdk-lib/aws-iam";
+
+export class RagCdkInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create a DynamoDB table to store the query data and results.
+    // const ragQueryTable = new Table(this, "QueriesTable", {
+    //   partitionKey: { name: "query_id", type: AttributeType.STRING },
+    //   billingMode: BillingMode.PAY_PER_REQUEST,
+    // });
+    
+    // Using the table already created in the previous step.
+    const ragQueryTable = Table.fromTableName(this, "ExistingQueriesTable", "RagCdkInfraStack-QueriesTable7395E8FA-17BGT2YQ1QX1F");
+
+    // Function to handle the API requests. Uses same base image, but different handler.
+    const apiImageCode = DockerImageCode.fromImageAsset("../image", {
+      cmd: ["app_api_handler.handler"],
+      buildArgs: {
+        platform: "linux/amd64",
+      },
+    });
+    const apiFunction = new DockerImageFunction(this, "ApiFunc", {
+      code: apiImageCode,
+      memorySize: 256,
+      timeout: cdk.Duration.seconds(30),
+      architecture: Architecture.X86_64,
+      environment: {
+        TABLE_NAME: ragQueryTable.tableName,
+      },
+    });
+
+    // Public URL for the API function.
+    const functionUrl = apiFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
+    // Grant permissions for all resources to work together.
+    ragQueryTable.grantReadWriteData(apiFunction);
+    apiFunction.role?.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AmazonBedrockFullAccess")
+    );
+
+    // Output the URL for the API function.
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: functionUrl.url,
+    });
+  }
+}
