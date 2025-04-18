8206f7e9f912a40da755f5de6c38e43164816a9a
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2025-04-18 18:28:41 -0400

    feat: deployed the backend to AWS Cloud, modified the deployment stack to include various information. The backend works perfectly on the hosted website now.

--- chatbot-infra/lib/chatbot-infra-stack.ts
+++ chatbot-infra/lib/chatbot-infra-stack.ts
@@ -12,8 +12,12 @@ import {
 import { ManagedPolicy } from "aws-cdk-lib/aws-iam";
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as dotenv from 'dotenv';
 
-export class RagCdkInfraStack extends cdk.Stack {
+dotenv.config();
+
+export class ChatbotInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -27,6 +31,10 @@ export class RagCdkInfraStack extends cdk.Stack {
     if (!apiKey) {
       throw new Error("API_KEY environment variable is not set");
     }
+    const pineconeIndexName = process.env.PINECONE_INDEX_NAME;
+    if (!pineconeIndexName) { 
+      throw new Error("PINECONE_INDEX_NAME environment variable is not set");
+    }
 
     // Create a DynamoDB table to store the query data and results.
     // const ragQueryTable = new Table(this, "QueriesTable", {
@@ -35,14 +43,20 @@ export class RagCdkInfraStack extends cdk.Stack {
     // });
     
     // Using the table already created in the previous step.
-    const ragQueryTable = Table.fromTableName(this, "ExistingQueriesTable", "RagCdkInfraStack-QueriesTable7395E8FA-17BGT2YQ1QX1F");
+    const ragQueryTable = Table.fromTableName(this, "ExistingQueriesTable", "lewas-chatbot-queries");
 
     // Create a new DynamoDB table for processed files
-    const processedFilesTable = new Table(this, "ProcessedFilesTable", {
-      tableName: "lewas-chatbot-processed-files",
-      partitionKey: { name: "filename", type: AttributeType.STRING },
-      billingMode: BillingMode.PAY_PER_REQUEST,
-    });
+    // const processedFilesTable = new Table(this, "ProcessedFilesTable", {
+    //   tableName: "lewas-chatbot-processed-files",
+    //   partitionKey: { name: "filename", type: AttributeType.STRING },
+    //   billingMode: BillingMode.PAY_PER_REQUEST,
+    // });
+
+    const PROCESSED_FILES_TABLE_NAME = process.env.PROCESSED_FILES_TABLE_NAME;
+    if (!PROCESSED_FILES_TABLE_NAME) {
+      throw new Error("DYNAMODB_TABLE_NAME_ANIMAL environment variable is not set");
+    }
+    const processedFilesTable = dynamodb.Table.fromTableName(this, 'lewas-chatbot-processed-files', PROCESSED_FILES_TABLE_NAME);
 
     // Function to handle the API requests. Uses same base image, but different handler.
     const apiImageCode = DockerImageCode.fromImageAsset("../image", {
@@ -56,7 +70,8 @@ export class RagCdkInfraStack extends cdk.Stack {
       environment: {
         TABLE_NAME: ragQueryTable.tableName,
         PINECONE_API_KEY: pineconeApiKey,
-        API_KEY: apiKey
+        API_KEY: apiKey,
+        PINECONE_INDEX_NAME: pineconeIndexName,
       },
     });
 
@@ -84,14 +99,14 @@ export class RagCdkInfraStack extends cdk.Stack {
 
     // Pemission for the API fucntion to access the S3 bucket.
     // Reference the existing S3 bucket
-    const existingBucket = s3.Bucket.fromBucketName(this, 'ExistingBucket', 'lewas-chatbot');
+    const existingBucket = s3.Bucket.fromBucketName(this, 'ExistingBucket', 'lewas-chatbot-v2');
 
     // Grant read/write permissions to the API function
     existingBucket.grantReadWrite(apiFunction);
 
 
     // Add S3 environment variables to both functions
-    apiFunction.addEnvironment('S3_BUCKET', 'lewas-chatbot');
+    apiFunction.addEnvironment('S3_BUCKET', 'lewas-chatbot-v2');
 
     apiFunction.role?.addManagedPolicy(
       ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess")
--- chatbot-infra/test/chatbot-infra.test.ts
+++ chatbot-infra/test/chatbot-infra.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as RagCdkInfra from '../lib/rag-cdk-infra-stack';
+// import * as ChatbotInfra from '../lib/chatbot-infra-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/rag-cdk-infra-stack.ts
+// example resource in lib/chatbot-infra-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new RagCdkInfra.RagCdkInfraStack(app, 'MyTestStack');
+//   const stack = new ChatbotInfra.ChatbotInfraStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
