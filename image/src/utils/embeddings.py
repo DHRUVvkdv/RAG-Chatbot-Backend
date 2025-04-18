411ae75df56690e21e7151d998314a81adb68762
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2025-04-18 15:11:30 -0400

    feat: succesfully initalized the model on AWS Bedrock console, connected to it and stored the response as well

--- image/src/utils/embeddings.py
+++ image/src/utils/embeddings.py
@@ -9,4 +9,7 @@ def get_embedding_function() -> Any:
     Returns:
         Any: An instance of BedrockEmbeddings.
     """
-    return BedrockEmbeddings()
+    return BedrockEmbeddings(
+        model_id="amazon.titan-embed-text-v2:0",  # Specified the titan model we would use
+        region_name="us-east-1",
+    )
